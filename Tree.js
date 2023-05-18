ac5f8c2e97d7f3843f86c842802c504bf879cb93
Joao <jonhlhn.surf@gmail.com>
2023-05-18 14:16:39 -0300

    Take in target square in Tree constructor

--- Tree.js
+++ Tree.js
@@ -1,7 +1,9 @@
 class Tree {
-	constructor(root) {
+	constructor(root, needle) {
 		(Array.isArray(root) && root.length === 2 && root[0] > -1 && root[0] < 8 && root[1] > -1 && root[1] < 8) 
-			? this.root = build(root)
+			? this.root = build(root, needle)
 			: this.root = null;
 	}
 }
+
+
