9db39972e2e2f6f022c4c642da065c4e23a5f561
Joao <jonhlhn.surf@gmail.com>
2023-05-18 14:17:46 -0300

    Remove checking in Tree contructor

--- Tree.js
+++ Tree.js
@@ -1,8 +1,6 @@
 class Tree {
 	constructor(root, needle) {
-		(Array.isArray(root) && root.length === 2 && root[0] > -1 && root[0] < 8 && root[1] > -1 && root[1] < 8) 
-			? this.root = build(root, needle)
-			: this.root = null;
+		this.root = build(root, needle)
 	}
 }
 
